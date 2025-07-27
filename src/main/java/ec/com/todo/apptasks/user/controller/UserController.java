d8897c6a2ce28a5eab3a5f5036c5c25ef949c21a
Mishell Yagual <mcym_1995@hotmail.com>
2025-07-27 13:47:06 -0500

    fix: Database dependency and controller setup

--- src/main/java/ec/com/todo/apptasks/user/controller/UserController.java
+++ src/main/java/ec/com/todo/apptasks/user/controller/UserController.java
@@ -7,11 +7,13 @@ import ec.com.todo.apptasks.user.dto.response.UserDTO;
 import ec.com.todo.apptasks.user.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/api/users")
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
