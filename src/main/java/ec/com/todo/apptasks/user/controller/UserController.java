5a0616c6580c444e5dd4966ce48905d29321db42
Mishell Yagual <mcym_1995@hotmail.com>
2025-10-11 15:58:48 -0500

    fix: Response Entity usage for handling better HTTP response codes for the Unit Testing focused in this section

--- src/main/java/ec/com/todo/apptasks/user/controller/UserController.java
+++ src/main/java/ec/com/todo/apptasks/user/controller/UserController.java
@@ -6,6 +6,9 @@ import ec.com.todo.apptasks.user.dto.request.UpdateUserDTO;
 import ec.com.todo.apptasks.user.dto.response.UserDTO;
 import ec.com.todo.apptasks.user.service.UserService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,23 +23,24 @@ public class UserController {
     }
 
     @GetMapping("/all")
-    public List<UserDTO> getAllUsers() {
-        return userService.getAll();
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAll());
     }
 
     @PostMapping("/create")
-    public void createUser(@Valid @RequestBody CreateUserDTO userDTO) {
-        userService.save(userDTO);
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody CreateUserDTO userDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userDTO));
     }
 
     @PostMapping("/update")
-    public void updateUser(@Valid @RequestBody UpdateUserDTO userDTO) {
-        userService.update(userDTO);
+    public ResponseEntity<UserDTO> updateUser(@Valid @RequestBody UpdateUserDTO userDTO) {
+        return ResponseEntity.ok(userService.update(userDTO));
     }
 
     @PostMapping("/delete")
-    public void deleteUser(@Valid @RequestBody DeleteUserDTO userDTO) {
+    public ResponseEntity<Void> deleteUser(@Valid @RequestBody DeleteUserDTO userDTO) {
         userService.delete(userDTO);
+        return ResponseEntity.accepted().build();
     }
 
 
